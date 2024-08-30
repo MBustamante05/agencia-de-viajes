20acc82ff6bc7aff7bd9027154be92fd014d7f72
MBustamante05 <securekali85@gmail.com>
2024-08-29 19:52:31 -0500

    Registro de usuario

---
+++ registro_usuario.py
@@ -0,0 +1,11 @@
+#Registrar usuario con nombre y contraseña
+import stdiomask #Libreria para ocultar contraseña
+
+nombre_usuario = input("Ingrese su nombre: ").lower()
+email_usuario = input("Ingrese su email: ")
+contraseña_usuario = stdiomask.getpass("Ingresa tu contraseña: ","*")
+
+print("\nHas sido registrado exitosamente!")
+#Mensaje de bienvenida al software
+print("\n---------------------------------")
+print(f"Bienvenido/a, {nombre_usuario}.")
